9d1bf03facc01b7f1c12dc0a6b0a847dafb7c022
lzw <1129459766@qq.com>
2018-03-20 18:12:22 +0800

    ..

--- src/main/java/lzw/iot/base/service/impl/AsyncTaskServiceImpl.java
+++ src/main/java/lzw/iot/base/service/impl/AsyncTaskServiceImpl.java
@@ -11,6 +11,8 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import static com.pi4j.wiringpi.Gpio.delay;
+
 /**
  * @author zzy
  * @date 2018/3/20 11:00
@@ -20,14 +22,22 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
 
     private final Log logger = LogFactory.getLog(getClass());
 
+    private static final int KEY_SHORT_PRESS = 1;
+
+    private static final int KEY_LONG_PRESS = 2;
+
+    private static final int KEY_LONG_TIMER = 3;
+
+    private long lastKeytime = 0;
+
     @Override
     @Async
-    public  void gpioListenerTask() {
+    public void gpioListenerTask() {
         final Console console = new Console();
 
         final GpioController gpio = GpioFactory.getInstance();
 
-//        console.promptForExit();
+        console.promptForExit();
         //按键GPIO
         Pin pin = CommandArgumentParser.getPin(
                 RaspiPin.class,
@@ -45,20 +55,52 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
 
         // 事件监听
         myButton.addListener((GpioPinListenerDigital) event -> {
-
-            long startTime = System.currentTimeMillis();
-            if (event.getState().isHigh()){
-                logger.info("开始配网...");
+            switch (keydown(event.getState().isHigh())) {
+                case KEY_SHORT_PRESS:
+                    logger.info("点按");
+                    break;
+                case KEY_LONG_PRESS:
+                    logger.info("开始配网...");
+                    break;
+                default:
+                    break;
             }
+
         });
 
-//        try {
-//            console.waitForExit();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            console.waitForExit();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // forcefully shutdown all GPIO monitoring threads and scheduled tasks
         gpio.shutdown();
+    }
+
+    /**
+     * 按键按下时间检测
+     *
+     * @param isHigh 是否为高电平
+     * @return
+     */
+    private int keydown(boolean isHigh) {
+        long keepTime;
+        if (isHigh) {
+            delay(100);
+            keepTime = System.currentTimeMillis() / 1000;
+            while (isHigh) {
+                if ((System.currentTimeMillis() / 1000 - keepTime) > KEY_LONG_TIMER) {
+                    lastKeytime = System.currentTimeMillis();
+                    return KEY_LONG_PRESS;
+                }
+            } //until open the key
+
+            if ((System.currentTimeMillis() / 1000 - lastKeytime) > KEY_LONG_TIMER) {
+                return KEY_SHORT_PRESS;
+            }
+            return 0;
+        }
+        return 0;
     }
 }
