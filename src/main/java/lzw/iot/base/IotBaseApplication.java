10dbbcf790652034804f53bc4b374ea99084b93e
lzw <1129459766@qq.com>
2018-03-21 16:59:25 +0800

    ..

--- src/main/java/lzw/iot/base/IotBaseApplication.java
+++ src/main/java/lzw/iot/base/IotBaseApplication.java
@@ -16,6 +16,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import static com.pi4j.wiringpi.Gpio.delay;
+
 
 @SpringBootApplication
 @EnableAsync
@@ -23,8 +25,16 @@ public class IotBaseApplication {
 
     private static final Log LOGGER = LogFactory.getLog(IotBaseApplication.class);
 
-//    @Autowired
-//    private AsyncTaskService asyncTaskService
+    private static final int KEY_SHORT_PRESS = 1;
+
+    private static final int KEY_LONG_PRESS = 2;
+
+    private static final int KEY_LONG_TIMER = 5;
+
+    private long lastKeytime = 0;
+
+    private PinState pinState;
+
 
     public static void main(String[] args) {
         LOGGER.info("\n========================================================="
@@ -37,8 +47,83 @@ public class IotBaseApplication {
                 + "\n=========================================================");
         SpringApplication.run(IotBaseApplication.class, args);
 
-        AsyncTaskServiceImpl asyncTaskService = new AsyncTaskServiceImpl();
-        asyncTaskService.gpioListenerTask();
+//        AsyncTaskServiceImpl asyncTaskService = new AsyncTaskServiceImpl();
+//        asyncTaskService.gpioListenerTask();
+        IotBaseApplication iotBaseApplication = new IotBaseApplication();
+        iotBaseApplication.gpioListenerTask();
+
+    }
+    public void gpioListenerTask() {
+        final Console console = new Console();
+
+        final GpioController gpio = GpioFactory.getInstance();
+
+        console.promptForExit();
+        //按键GPIO
+        Pin pin = CommandArgumentParser.getPin(
+                RaspiPin.class,
+                RaspiPin.GPIO_02);
+
+        // 默认按键方式
+        PinPullResistance pull = CommandArgumentParser.getPinPullResistance(
+                PinPullResistance.PULL_DOWN);
+
+        // 按键事件
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(pin, pull);
+
+        // 程序退出时，释放引脚
+        myButton.setShutdownOptions(true);
+
+        // 事件监听
+        myButton.addListener((GpioPinListenerDigital) event -> {
+            LOGGER.info(event.getState());
+            this.pinState = event.getState();
+            switch (keydown()) {
+                case KEY_SHORT_PRESS:
+                    LOGGER.info("点按");
+                    break;
+                case KEY_LONG_PRESS:
+                    LOGGER.info("开始配网...");
+                    break;
+                default:
+                    LOGGER.info("qita");
+                    break;
+            }
+        });
+
+        try {
+            console.waitForExit();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // forcefully shutdown all GPIO monitoring threads and scheduled tasks
+        gpio.shutdown();
+
+    }
+
+    /**
+     * 按键按下时间检测
+     *
+     * @return
+     */
+    private synchronized int keydown() {
+        long keepTime;
+        if (this.pinState == PinState.HIGH) {
+            delay(100);
+            keepTime = System.currentTimeMillis() / 1000;
+            while (this.pinState == PinState.HIGH) {
+                if ((System.currentTimeMillis() / 1000 - keepTime) > KEY_LONG_TIMER) {
+                    lastKeytime = System.currentTimeMillis();
+                    return KEY_LONG_PRESS;
+                }
+            } //until open the key
+
+            if ((System.currentTimeMillis() / 1000 - lastKeytime) > KEY_LONG_TIMER) {
+                return KEY_SHORT_PRESS;
+            }
+            return 0;
+        }
+        return 0;
     }
 
 
