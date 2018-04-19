7d6887932aea8cdeba2ac1d89dcb19a3773c16d1
lzw <1129459766@qq.com>
2018-04-20 03:30:50 +0800

    ..

--- src/main/java/lzw/iot/base/event/listener/RGBChangeEventListener.java
+++ src/main/java/lzw/iot/base/event/listener/RGBChangeEventListener.java
@@ -3,7 +3,6 @@ package lzw.iot.base.event.listener;
 import de.pi3g.pi.rgbled.PinLayout;
 import de.pi3g.pi.rgbled.RGBLed;
 import lzw.iot.base.event.RGBChangeEvent;
-import lzw.iot.base.model.RgbEventType;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -22,6 +21,7 @@ public class RGBChangeEventListener implements ApplicationListener<RGBChangeEven
 
     private RGBLed rgbLed = new RGBLed(PinLayout.PIBORG_LEDBORG);
 
+    private boolean isConnecting;
     @Async
     @Override
     public void onApplicationEvent(RGBChangeEvent rgbChangeEvent) {
@@ -29,25 +29,23 @@ public class RGBChangeEventListener implements ApplicationListener<RGBChangeEven
 
         switch (rgbChangeEvent.getRgbEventType()) {
             case CONNECTED_WIFI:
+                isConnecting = false;
                 rgbLed.displayColor(Color.GREEN);
                 break;
             case CONNECTING_WIFI:
                 do {
-                    if (rgbChangeEvent.getRgbEventType() == RgbEventType.CONNECTING_WIFI){
-                        rgbLed.displayColor(Color.BLUE);
-                        try {
-                            Thread.sleep(1000L);
-                            rgbLed.off();
-                            Thread.sleep(1000L);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }else {
-                        break;
+                    rgbLed.displayColor(Color.BLUE);
+                    try {
+                        Thread.sleep(1000L);
+                        rgbLed.off();
+                        Thread.sleep(1000L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                } while (true);
+                } while (isConnecting);
                 break;
             case DISCONNECTED_WIFI:
+                isConnecting = false;
                 rgbLed.displayColor(Color.RED);
                 break;
             default:
