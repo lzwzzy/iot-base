82226c71357fa3266bb68c385072ce9d2459e4cc
lzw <1129459766@qq.com>
2018-05-17 19:00:03 +0800

    ..

--- src/main/java/lzw/iot/base/service/impl/AsyncTaskServiceImpl.java
+++ src/main/java/lzw/iot/base/service/impl/AsyncTaskServiceImpl.java
@@ -1,5 +1,6 @@
 package lzw.iot.base.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.pi4j.component.button.Button;
 import com.pi4j.component.button.ButtonHoldListener;
 import com.pi4j.component.button.impl.GpioButtonComponent;
@@ -7,8 +8,9 @@ import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.system.NetworkInfo;
-import com.pi4j.system.SystemInfo;
 import com.pi4j.util.CommandArgumentParser;
+import de.pi3g.pi.rgbled.PinLayout;
+import de.pi3g.pi.rgbled.RGBLed;
 import lzw.iot.base.common.ErrorCode;
 import lzw.iot.base.event.RGBChangeEvent;
 import lzw.iot.base.exception.LemonException;
@@ -19,13 +21,16 @@ import lzw.iot.base.service.WifiControlService;
 import lzw.iot.base.util.I2CLcdDisplay;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.Map;
 
 import static com.pi4j.wiringpi.Gpio.*;
 
@@ -39,6 +44,8 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
 
     private final Log logger = LogFactory.getLog(getClass());
 
+    private RGBLed rgbLed = new RGBLed(PinLayout.PIBORG_LEDBORG);
+
     @Autowired
     private WifiControlService wifiControlService;
 
@@ -48,7 +55,13 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
     @Autowired
     private Mqtt mqtt;
 
+    private MqttClient mqttClient;
+
     private Button button;
+
+
+    @Value("${device-id}")
+    private String deviceId;
 
     /**
      * 主状态按键
@@ -89,6 +102,9 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
     public void gpioListenerTask() {
         //当前线程退出时，结束按键扫描
         Runtime.getRuntime().addShutdownHook(new Thread(() -> exiting = true));
+
+        //获取mqtt客户端
+        mqttClient = mqtt.getClient();
         //按键1GPIO
         Pin pin = CommandArgumentParser.getPin(
                 RaspiPin.class,
@@ -108,7 +124,6 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
         final GpioPinDigitalInput myButton2 = gpio.provisionDigitalInputPin(pin2, pull);
 
 
-
         GpioButtonComponent gpioButtonComponent = new GpioButtonComponent(myButton2);
         gpioButtonComponent.addListener(3000, (ButtonHoldListener) buttonEvent -> logger.debug("这个键你按了3s以上了"));
 
@@ -121,10 +136,69 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
         });
 
         try {
+            //初始化lcd1602
             int address = 0x27;
-            I2CLcdDisplay i2CLcdDisplay = new I2CLcdDisplay(address,I2CBus.BUS_1);
-            i2CLcdDisplay.outputToDisplay("hello","world", true);
+            I2CLcdDisplay i2CLcdDisplay = new I2CLcdDisplay(address, I2CBus.BUS_1);
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            //订阅网关控制事件
+            mqttClient.subscribe("device/event/gateway", 1);
+            mqttClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable throwable) {
+
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                    String payload = new String(mqttMessage.getPayload());
+                    Map map = JSON.parseObject(payload, Map.class);
+                    logger.info("接收消息主题 : " + topic);
+                    logger.info("接收消息Qos : " + mqttMessage.getQos());
+                    logger.info("接收消息内容 : " + payload);
+                    logger.info("==========================================================");
+                    if (deviceId.equals(map.get("deviceId"))) {
+                        Map event = JSON.parseObject(JSON.toJSON(map).toString(), Map.class);
+                        if (Boolean.parseBoolean(String.valueOf(event.get("warn")))) {
+                            //打开人体热释电感应
+                        } else {
+                            //关闭热释电感应
+                        }
+
+                        switch (event.get("color").toString()) {
+                            case "red":
+                                rgbLed.displayColor(Color.RED);
+                                break;
+                            case "orange":
+                                rgbLed.displayColor(Color.ORANGE);
+                                break;
+                            case "yellow":
+                                rgbLed.displayColor(Color.YELLOW);
+                                break;
+                            case "green":
+                                rgbLed.displayColor(Color.GREEN);
+                                break;
+                            case "cyan":
+                                rgbLed.displayColor(Color.CYAN);
+                                break;
+                            case "magenta":
+                                rgbLed.displayColor(Color.MAGENTA);
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+                }
+            });
+        } catch (MqttException e) {
             e.printStackTrace();
         }
         try {
@@ -140,8 +214,7 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
                         //微信配网
                         wifiControlService.airkiss_connect_wifi();
                         //配网成功后订阅绑定事件主题
-                        MqttClient client = mqtt.getClient();
-                        client.subscribe("device/event", 1);
+                        mqttClient.subscribe("device/event", 1);
                         break;
                     default:
                         break;
@@ -160,9 +233,9 @@ public class AsyncTaskServiceImpl implements AsyncTaskService {
     @Async
     public void wifiStatusScan() {
         try {
-            if(NetworkInfo.getIPAddress() != null){
+            if (NetworkInfo.getIPAddress() != null) {
                 applicationContext.publishEvent(new RGBChangeEvent(this, RgbEventType.CONNECTED_WIFI));
-            }else {
+            } else {
                 applicationContext.publishEvent(new RGBChangeEvent(this, RgbEventType.DISCONNECTED_WIFI));
             }
         } catch (IOException | InterruptedException e) {
